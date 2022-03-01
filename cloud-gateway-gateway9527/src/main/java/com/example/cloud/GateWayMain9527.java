d4a7aa0deb6eb52bf3e6bd2ddf4663e89f3f677b
szz <1043375957@qq.com>
2022-03-01 09:15:27 +0800

    fix(commons): 忽略

--- cloud-gateway-gateway9527/src/main/java/com/example/cloud/GateWayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/example/cloud/GateWayMain9527.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
+/**
+ * @author CHINA
+ */
+
 @SpringBootApplication
 @EnableEurekaClient
 public class GateWayMain9527 {
-
     public static void main(String[] args) {
         SpringApplication.run(GateWayMain9527.class, args);
     }
